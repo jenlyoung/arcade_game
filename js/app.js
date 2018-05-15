259f622b53323c4748f42c7e616f0476613a2dd0
Jennifer Young <jen.l.young@gmail.com>
2018-05-15 18:56:52 -0500

    Feat: modal play again button working

--- js/app.js
+++ js/app.js
@@ -1,51 +1,68 @@
+//Score Object
 
-var Score = function (){
+// sets initial score to 500
+var Score = function () {
     this.score = 500;
 };
 
-Score.prototype.startScore = function(){
+//appends score to html when game starts/restart
+//empties the html so that the score is cleared and new score is added
+Score.prototype.displayScore = function () {
     $(".score").empty().append(this.score);
 };
 
-Score.prototype.earnPoints = function() {
+//increases the score by 100 when sprite reaches water
+Score.prototype.earnPoints = function () {
     this.score += 100;
-
-    // if (this.score > 1000){
-    //     this.score = 1000;
-    // }
 };
 
-Score.prototype.loosePoints = function() {
+// decreases the points by 100 when sprite get hits by bug
+Score.prototype.loosePoints = function () {
     this.score -= 100;
-    // if (this.score < 0){
-    //     this.score = 0;
-    // }
+
 };
 
-Score.prototype.win = function() {
-    if (this.score === 600) {
+//resets the score to 500
+Score.prototype.reset = function () {
+    this.score = 500;
+    this.displayScore();
+};
+
+// win modal pops up when the score is 1000
+Score.prototype.win = function () {
+    if (this.score === 700) {
+        console.log("win", this);
         $(".modal-title").empty().append("Congratulations!");
         $(".modal-body").empty().append("<p>You Win!</p>");
         $("#myModal").modal("show");
-        this.reset();
+
+        var scope = this;
+        $("#play-again").on('click', function () {
+            console.log("button", this);
+            scope.reset();
+        });
     }
 };
 
-Score.prototype.loose = function() {
-    if (this.score === 400){
+//loose modal pops up when score reaches 0
+Score.prototype.loose = function () {
+    if (this.score === 300) {
+        console.log("loose:", this);
         $(".modal-title").empty().append("Sorry!");
         $(".modal-body").empty().append("<p>You Loose!</p>");
         $("#myModal").modal("show");
-        this.reset();
+
+        var scope = this;
+        $("#play-again").on('click', function () {
+            scope.reset();
+        });
     }
 };
 
-Score.prototype.reset = function() {
-    this.score = 500;
-}
+//Enemy Object
 
 // Enemies our player must avoid
-var Enemy = function (x, y) {
+var Enemy = function (x, y, score) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -57,6 +74,7 @@ var Enemy = function (x, y) {
     this.h = 67;
     this.speed = Math.floor((Math.random() * 200) + 100);
     this.sprite = 'images/enemy-bug.png';
+    this.score = score;
 };
 
 // Update the enemy's position, required method for game
@@ -66,31 +84,7 @@ Enemy.prototype.update = function (dt) {
         this.x = this.x + this.speed * dt;
     }
     else this.x = -20;
-
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
 };
-
-function drawBox(x, y, width, height, color) {
-    ctx.beginPath();
-    ctx.rect(x, y, width, height);
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = color;
-    ctx.stroke();
-}
-
-// Enemy.prototype.collision = function (player) {
-//     allEnemies.forEach(function (enemy) {
-//         if (player.x < this.x + this.w &&
-//         player.x + player.w > this.x &&
-//         player.y < this.y + this.h &&
-//         player.h + player.y > this.y) {
-//         // collision detected!
-//         alert ("collision");
-//     }
-// };
-
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function () {
@@ -98,23 +92,29 @@ Enemy.prototype.render = function () {
     // drawBox(this.x, this.y + 77, 100, 67, "yellow");
 };
 
+Enemy.prototype.stopBugs = function () {
+    this.speed = 0;
+};
+
+//Player Class
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function (score) {
+var Player = function (score, enemies) {
     this.sprite = 'images/char-boy.png';
     this.x = 200;
     this.y = 400;
     this.w = 65;
     this.h = 75;
     this.score = score;
+    this.enemies = enemies;
 };
 
 Player.prototype.update = function () {
-    player.checkBoundaries();
     player.collision();
-    player.winGame();
+    player.checkBoundaries();
+    player.hitWater();
 };
 
 //
@@ -138,10 +138,11 @@ Player.prototype.handleInput = function (direction) {
     }
     if (direction === 'down') {
         this.y = this.y + verticalSize;
-        console.log(this.y);
+        // console.log(this.y);
     }
 };
 
+//does not let player go out of bounds
 Player.prototype.checkBoundaries = function () {
     if (this.y >= 400) {
         this.y = 400;
@@ -154,6 +155,7 @@ Player.prototype.checkBoundaries = function () {
     }
 };
 
+//checks for collisions
 Player.prototype.collision = function () {
     // console.log(allEnemies);
     // var scope = this;
@@ -164,60 +166,67 @@ Player.prototype.collision = function () {
             this.x + this.w > enemy.x &&
             this.y < enemy.y + enemy.h &&
             this.h + this.y > enemy.y) {
-            // enemy.speed = 0;
-            this.startPosition();
-            // this.score.updateScore();
+            // subtract points when hit and display score
             this.score.loosePoints();
+            this.score.displayScore();
+
+            //sprite goes to start position
+            this.startPosition();
+
+            // points = 0, loose game modal
             this.score.loose();
+
+            // bugs stop moving
+            this.looseGame();
         }
     }, this);
 };
 
-// Enemy.prototype.collision = function (player) {
-//     allEnemies.forEach(function (enemy) {
-//         if (player.x < enemy.x + enemy.w &&
-//             player.x + player.w > enemy.x &&
-//             player.y < enemy.y + enemy.h &&
-//             player.h + player.y > enemy.y) {
-//             // collision detected!
-//             alert ("collision");
-//         }
-//     });
-// };
-
-Player.prototype.startPosition = function() {
+// //sets starting position for player
+Player.prototype.startPosition = function () {
     this.x = 200;
     this.y = 400;
-    this.score.startScore();
 };
 
-Player.prototype.winGame = function() {
-    if (this.y < 50){
+Player.prototype.hitWater = function () {
+    if (this.y < 50) {
         this.score.earnPoints();
+        this.score.displayScore();
+
         this.startPosition();
-        this.score.win();
+        // this.score.win();
+        this.winGame();
     }
 };
 
-//
-// var Modal = function (score){
-//     this.winMessage = "Congratulations! You won!",
-//     this.looseMessage = "You Loose!",
-//     this.placeMessage = function () {
-//         if (this.score === 600){
-//             $(".modal-title").append(this.winMessage());
-//         }
-//     }
-//
+Player.prototype.winGame = function () {
+    this.score.win();
+    // this.enemy.speed = 0;
+    // this.enemies.forEach(function (enemy) {
+    //     enemy.stopBugs();
+    // });
+};
+
+Player.prototype.looseGame = function () {
+    this.score.loose();
+    // this.enemy.speed = 0;
+    // this.enemies.forEach(function (enemy) {
+    //     enemy.stopBugs();
+    // });
+};
+
+// Player.prototype.stopBugs = function () {
+//     this.enemy.speed = 0;
 // };
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [new Enemy(100, 220), new Enemy(0, 140), new Enemy(50, 58)];
+var allEnemies = ([new Enemy(100, 220), new Enemy(0, 140), new Enemy(50, 58)]);
 
 var score = new Score();
-var player = new Player(score);
+var player = new Player(score, allEnemies);
 // var modal = new Modal(score);
 
 
@@ -234,4 +243,40 @@ document.addEventListener('keyup', function (e) {
     player.handleInput(allowedKeys[e.keyCode]);
 });
 
+
+//
+
+
+//extras
+
+// Enemy.prototype.collision = function (player) {
+//     allEnemies.forEach(function (enemy) {
+//         if (player.x < this.x + this.w &&
+//         player.x + player.w > this.x &&
+//         player.y < this.y + this.h &&
+//         player.h + player.y > this.y) {
+//         // collision detected!
+//         alert ("collision");
+//     }
+// };
+
+function drawBox(x, y, width, height, color) {
+    ctx.beginPath();
+    ctx.rect(x, y, width, height);
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = color;
+    ctx.stroke();
+}
+
+// Enemy.prototype.collision = function (player) {
+//     allEnemies.forEach(function (enemy) {
+//         if (player.x < enemy.x + enemy.w &&
+//             player.x + player.w > enemy.x &&
+//             player.y < enemy.y + enemy.h &&
+//             player.h + player.y > enemy.y) {
+//             // collision detected!
+//             alert ("collision");
+//         }
+//     });
+// };
 
